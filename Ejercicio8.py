633e58a2b68b3cad09578e6dbf826020bbd3e312
Antonio Pérez Galán <49763952+Antoino1997@users.noreply.github.com>
2025-10-25 13:51:32 +0200

    Fix order of operations in average calculation
    Added parentheses to ensure correct order of operations for average calculation.

--- Ejercicio8.py
+++
@@ -1,7 +0,0 @@
-nota1 = int(input("Introduzca la primera nota: "))
-nota2 = int(input("Introduzca la segunda nota: "))
-nota3 = int(input("Introduzca la tercera nota: "))
-
-promedio = (nota1 + nota2 + nota3) / 3
-
-print(f"El promedio de las  3 notas es {promedio}")
