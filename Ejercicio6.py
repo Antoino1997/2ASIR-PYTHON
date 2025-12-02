2eca791df0ba79a1c4c3c7743f24ae26654b4045
Antonio Pérez Galán <49763952+Antoino1997@users.noreply.github.com>
2025-12-02 11:33:05 +0100

    Delete Ejercicio6.py

--- Ejercicio6.py
+++
@@ -1,7 +0,0 @@
-"""Tupla con los datos de una persona, que se desempaqueta y se imprime con el formato establecido."""
-persona = ("Ana", 25 , "Madrid")
-nombre, edad, ciudad = persona
-
-print("Nombre:", nombre)
-print("Edad:", edad)
-print("Ciudad:", ciudad)
